332d392886216f208ad1abfdee77d2c81bf333ce
Natalia Canales <nataliacanales95@outlook.com>
2021-01-31 19:14:20 +0100

    Finished part 2

--- robotGame.js
+++ robotGame.js
@@ -15,3 +15,21 @@ function gridCreator(rows = 10, columns = 10) {
 
     return grid;
 }
+
+let grid = gridCreator(8, 8);
+let obstacles = [
+    {x: 2, y: 6},
+    {x: 3, y: 1},
+    {x: 7, y: 2}
+];
+
+function setObstacles(grid, obstacles) {
+    obstacles.forEach(object => {
+        if ((object.x < grid.length) && (object.y < grid[0].length)
+            && (object.x >= 0) && (object.y >= 0)){
+            grid[object.x][object.y] = true;
+        } else {
+            console.log(`Cannot set obstacle at x: ${object.x}, y: ${object.y}`);
+        }
+    });
+}
